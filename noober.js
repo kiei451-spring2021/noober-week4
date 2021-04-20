8e54f689b5ad09b33ae27faa9576a05cb7e2bf2d
Brian Eng <brian@brianeng.com>
2021-04-19 19:26:20 -0500

    initial commit

---
+++ noober.js
@@ -0,0 +1,13 @@
+async function pageLoaded() {
+  let response = await fetch('https://kiei451.com/api/rides.json')
+  let json = await response.json()
+
+  // writes the returned JSON to the console
+  console.dir(json)
+  
+  // 🔥 start here: write the recipe (algorithm), then write the code
+  
+}
+
+window.addEventListener('DOMContentLoaded', pageLoaded)
+
